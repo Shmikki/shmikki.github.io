b37c15d430cba75d4d4a50027495f21db27df839
Shmikki <shmikki407@gmail.com>
2020-11-08 00:02:40 +0300

    fixed video button

--- Berlin/js/request.js
+++ Berlin/js/request.js
@@ -4,17 +4,17 @@ const currencyToHTML = currency => `
         <div class="currency__rate">${currency.rate} BYN</div>
     </div>
 `,
-currencyNames = ['USD','EUR','RUB'];
-
-
-
-
+currencyNames = ['USD','EUR','RUB'],
+currencyBlock = document.querySelector('#currency');
 
 fetch('https://developerhub.alfabank.by:8273/partner/1.0.0/public/nationalRates')
-.then(response => response.json())
+.then(
+    response => response.json()
+)
 .then(valuta => {
     let currencies = currencyNames.map(currencyName => valuta.rates.find(value => value.iso === currencyName));
     const html = currencies.map(currencyToHTML).join('');
-    console.log(currencies);
-    document.querySelector('#currency').innerHTML = html;
-});
+    currencyBlock.innerHTML = html;
+    }
+)
+.catch(err => console.log(err));
