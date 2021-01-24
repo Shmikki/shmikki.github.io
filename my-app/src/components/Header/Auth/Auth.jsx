2448b4db953c30b3e97bddfcbd11aa73763cf09e
Shmikki <shmikki407@gmail.com>
2021-01-24 17:50:51 +0300

    switch repo for react-app

--- my-app/src/components/Header/Auth/Auth.jsx
+++
@@ -1,9 +0,0 @@
-import  {Redirect , NavLink} from "react-router-dom";
-
-const Auth = (props) => {
-    return (
-        props.userID ? <span onClick={() => {props.logOut();  <Redirect to={"/login"} />} }>Logout</span> : <NavLink to={"login"}>Login </NavLink>
-    )
-}
-
-export default Auth;
